f8c4fcf2bcce78942fb2f2c86c298403aa5f0566
alimbdit <alimbdit@gmail.com>
2023-04-16 04:49:00 +0600

    sign up and sign in added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqkAyIeAECdj3pgnbZ329kTjGZTdDBhr8",
+  authDomain: "auth-firebase-context-ta-ac7fc.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-ac7fc",
+  storageBucket: "auth-firebase-context-ta-ac7fc.appspot.com",
+  messagingSenderId: "77123769873",
+  appId: "1:77123769873:web:eee880a2ce44d664a283b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
